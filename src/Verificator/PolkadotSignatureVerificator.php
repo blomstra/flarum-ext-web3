c682ac8ab01ca2ea97a0f1f87e81ff81e83e8e0a
Sami Mazouz <ilyasmazouz@gmail.com>
2022-08-04 14:46:38 +0100

    chore: add note about backend polkadot verification

--- src/Verificator/PolkadotSignatureVerificator.php
+++ src/Verificator/PolkadotSignatureVerificator.php
@@ -5,6 +5,9 @@ namespace Blomstra\Web3\Verificator;
 use Blomstra\Web3\sr25519\sr25519;
 use function Blomstra\Web3\ffiIsEnabled;
 
+/**
+ * @TODO doesn't currently work, figure out or switch to nodejs on the backend.
+ */
 class PolkadotSignatureVerificator implements SignedMessageVerificator
 {
     public function verifySignature(string $message, string $signature, string $publicKey): bool
